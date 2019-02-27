fd814933db3886beb160dca6a355e1c68df07d2a
Zoran Marjanovic <zmarjanovic89@gmail.com>
2019-02-27 21:26:22 +0100

    Added missing icons for challenges

---
+++ icons/IconLayers.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react'
+import Icon from './Icon'
+import type {IconProps} from './Icon'
+
+class IconLayers extends Icon {
+  constructor(props: IconProps) {
+    super(props)
+
+    this.name = 'layers-2'
+  }
+}
+
+export default IconLayers
