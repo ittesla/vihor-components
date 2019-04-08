1b3f704c0c59185a4a97242d5dd96d6f786d0e12
Zoran Marjanovic <zmarjanovic89@gmail.com>
2019-04-08 23:39:07 +0200

    Added new icons and prop for setting inactive checkbox color

---
+++ icons/IconStopFilled.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react'
+import Icon from './Icon'
+import type {IconProps} from './Icon'
+
+class IconStopFilled extends Icon {
+  constructor(props: IconProps) {
+    super(props)
+
+    this.name = 'group--22'
+  }
+}
+
+export default IconStopFilled
